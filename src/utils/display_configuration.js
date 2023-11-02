286c5e0fb4be42c631df64d635c6b0c5cb1e3d6c
Administrator <Administrator@172.31.36.201>
2023-11-02 15:26:25 +0000

    initial commit

---
+++ src/utils/display_configuration.js
@@ -0,0 +1,41 @@
+export const productViewConfig = {
+Details: [
+    {
+      key: "Product_id",
+      value: "Product_id",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Name",
+      value: "Name",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Color",
+      value: "Color",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Product_description",
+      value: "Product_description",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Manufacturer",
+      value: "Manufacturer",
+      type: "text",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+  ],
+}
